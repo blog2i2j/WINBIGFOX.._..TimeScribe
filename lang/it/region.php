db0060a125f47eccff1007f061c271f2337a8127
Loacky <lorenzoadacher@gmail.com>
2026-01-02 13:04:07 +0100

    feat(lang): update Italian translations for regions and validation messages

--- lang/it/region.php
+++ lang/it/region.php
@@ -5,35 +5,35 @@ declare(strict_types=1);
 return [
     // Australia
     'AU' => 'Australia',
-    'AU-ACT' => 'Australia - Australian Capital Territory',
-    'AU-NSW' => 'Australia - New South Wales',
-    'AU-NT' => 'Australia - Northern Territory',
+    'AU-ACT' => 'Australia - Territorio della Capitale Australiana',
+    'AU-NSW' => 'Australia - Nuovo Galles del Sud',
+    'AU-NT' => 'Australia - Territorio del Nord',
     'AU-QLD' => 'Australia - Queensland',
-    'AU-SA' => 'Australia - South Australia',
+    'AU-SA' => 'Australia - Australia Meridionale',
     'AU-TAS' => 'Australia - Tasmania',
     'AU-VIC' => 'Australia - Victoria',
-    'AU-WA' => 'Australia - Western Australia',
+    'AU-WA' => 'Australia - Australia Occidentale',
 
     // Austria
     'AT' => 'Austria',
     'AT-1' => 'Austria - Burgenland',
-    'AT-2' => 'Austria - Carinthia',
-    'AT-3' => 'Austria - Lower Austria',
-    'AT-4' => 'Austria - Upper Austria',
-    'AT-5' => 'Austria - Salzburg',
-    'AT-6' => 'Austria - Styria',
-    'AT-7' => 'Austria - Tyrol',
+    'AT-2' => 'Austria - Carinzia',
+    'AT-3' => 'Austria - Bassa Austria',
+    'AT-4' => 'Austria - Alta Austria',
+    'AT-5' => 'Austria - Salisburgo',
+    'AT-6' => 'Austria - Stiria',
+    'AT-7' => 'Austria - Tirolo',
     'AT-8' => 'Austria - Vorarlberg',
     'AT-9' => 'Austria - Vienna',
 
     // Belarus
-    'BY' => 'Belarus',
+    'BY' => 'Bielorussia',
 
     // Belgium
-    'BE' => 'Belgium',
+    'BE' => 'Belgio',
 
     // Brazil
-    'BR' => 'Brazil',
+    'BR' => 'Brasile',
 
     // Bulgaria
     'BG' => 'Bulgaria',
@@ -41,152 +41,152 @@ return [
     // Canada
     'CA' => 'Canada',
     'CA-AB' => 'Canada - Alberta',
-    'CA-BC' => 'Canada - British Columbia',
+    'CA-BC' => 'Canada - Columbia Britannica',
     'CA-MB' => 'Canada - Manitoba',
     'CA-NB' => 'Canada - New Brunswick',
-    'CA-NL' => 'Canada - Newfoundland and Labrador',
-    'CA-NT' => 'Canada - Northwest Territories',
-    'CA-NS' => 'Canada - Nova Scotia',
+    'CA-NL' => 'Canada - Terranova e Labrador',
+    'CA-NT' => 'Canada - Territori del Nord-Ovest',
+    'CA-NS' => 'Canada - Nuova Scozia',
     'CA-NU' => 'Canada - Nunavut',
     'CA-ON' => 'Canada - Ontario',
-    'CA-PE' => 'Canada - Prince Edward Island',
+    'CA-PE' => 'Canada - Isola del Principe Edoardo',
     'CA-QC' => 'Canada - Quebec',
     'CA-SK' => 'Canada - Saskatchewan',
     'CA-YT' => 'Canada - Yukon',
 
     // Czech Republic
-    'CZ' => 'Czech Republic',
+    'CZ' => 'Repubblica Ceca',
 
     // Denmark
-    'DK' => 'Denmark',
+    'DK' => 'Danimarca',
 
     // Estonia
     'EE' => 'Estonia',
 
     // Finland
-    'FI' => 'Finland',
+    'FI' => 'Finlandia',
 
     // France
-    'FR' => 'France',
-    'FR-57' => 'France - Moselle',
-    'FR-67' => 'France - Bas-Rhin',
-    'FR-68' => 'France - Haut-Rhin',
-    'FR-GF' => 'France - French Guiana',
-    'FR-GUA' => 'France - Guadeloupe',
-    'FR-LRE' => 'France - La Réunion',
-    'FR-MQ' => 'France - Martinique',
+    'FR' => 'Francia',
+    'FR-57' => 'Francia - Mosella',
+    'FR-67' => 'Francia - Basso Reno',
+    'FR-68' => 'Francia - Alto Reno',
+    'FR-GF' => 'Francia - Guyana francese',
+    'FR-GUA' => 'Francia - Guadalupa',
+    'FR-LRE' => 'Francia - La Réunion',
+    'FR-MQ' => 'Francia - Martinica',
 
     // Germany
-    'DE' => 'Germany',
-    'DE-BB' => 'Germany - Brandenburg',
-    'DE-BE' => 'Germany - Berlin',
-    'DE-BW' => 'Germany - Baden-Württemberg',
-    'DE-BY' => 'Germany - Bavaria',
-    'DE-HB' => 'Germany - Bremen',
-    'DE-HE' => 'Germany - Hesse',
-    'DE-HH' => 'Germany - Hamburg',
-    'DE-MV' => 'Germany - Mecklenburg-Western Pomerania',
-    'DE-NI' => 'Germany - Lower Saxony',
-    'DE-NW' => 'Germany - North Rhine-Westphalia',
-    'DE-RP' => 'Germany - Rhineland-Palatinate',
-    'DE-SH' => 'Germany - Schleswig-Holstein',
-    'DE-SL' => 'Germany - Saarland',
-    'DE-SN' => 'Germany - Saxony',
-    'DE-ST' => 'Germany - Saxony-Anhalt',
-    'DE-TH' => 'Germany - Thuringia',
+    'DE' => 'Germania',
+    'DE-BB' => 'Germania - Brandeburgo',
+    'DE-BE' => 'Germania - Berlino',
+    'DE-BW' => 'Germania - Baden-Württemberg',
+    'DE-BY' => 'Germania - Baviera',
+    'DE-HB' => 'Germania - Brema',
+    'DE-HE' => 'Germania - Assia',
+    'DE-HH' => 'Germania - Amburgo',
+    'DE-MV' => 'Germania - Meclemburgo-Pomerania Anteriore',
+    'DE-NI' => 'Germania - Bassa Sassonia',
+    'DE-NW' => 'Germania - Renania Settentrionale-Vestfalia',
+    'DE-RP' => 'Germania - Renania-Palatinato',
+    'DE-SH' => 'Germania - Schleswig-Holstein',
+    'DE-SL' => 'Germania - Saarland',
+    'DE-SN' => 'Germania - Sassonia',
+    'DE-ST' => 'Germania - Sassonia-Anhalt',
+    'DE-TH' => 'Germania - Turingia',
 
     // Greenland
-    'GL' => 'Greenland',
+    'GL' => 'Groenlandia',
 
     // Iceland
-    'IS' => 'Iceland',
+    'IS' => 'Islanda',
 
     // Ireland
-    'IE' => 'Ireland',
+    'IE' => 'Irlanda',
 
     // Italy
-    'IT' => 'Italy',
-    'IT-32' => 'Italy - South Tyrol',
+    'IT' => 'Italia',
+    'IT-32' => 'Italia - Alto Adige',
 
     // Latvia
-    'LV' => 'Latvia',
+    'LV' => 'Lettonia',
 
     // Liechtenstein
     'FL' => 'Liechtenstein',
 
     // Lithuania
-    'LT' => 'Lithuania',
+    'LT' => 'Lituania',
 
     // Luxembourg
-    'LU' => 'Luxembourg',
+    'LU' => 'Lussemburgo',
 
     // Mexico
-    'MX' => 'Mexico',
+    'MX' => 'Messico',
 
     // Netherlands
-    'NL' => 'Netherlands',
+    'NL' => 'Paesi Bassi',
 
     // Norway
-    'NO' => 'Norway',
+    'NO' => 'Norvegia',
 
     // Poland
-    'PL' => 'Poland',
+    'PL' => 'Polonia',
 
     // Portugal
-    'PT' => 'Portugal',
-    'PT-20' => 'Portugal - Azores',
-    'PT-30' => 'Portugal - Madeira',
+    'PT' => 'Portogallo',
+    'PT-20' => 'Portogallo - Azzorre',
+    'PT-30' => 'Portogallo - Madeira',
 
     // Russia
     'RU' => 'Russia',
 
     // Spain
-    'ES' => 'Spain',
+    'ES' => 'Spagna',
 
     // Sweden
-    'SE' => 'Sweden',
+    'SE' => 'Svezia',
 
     // Switzerland
-    'CH' => 'Switzerland',
-    'CH-AG' => 'Switzerland - Aargau',
-    'CH-AI' => 'Switzerland - Appenzell Innerrhoden',
-    'CH-AR' => 'Switzerland - Appenzell Ausserrhoden',
-    'CH-BE' => 'Switzerland - Bern',
-    'CH-BL' => 'Switzerland - Basel-Landschaft',
-    'CH-BS' => 'Switzerland - Basel-Stadt',
-    'CH-FR' => 'Switzerland - Fribourg',
-    'CH-GE' => 'Switzerland - Geneva',
-    'CH-GL' => 'Switzerland - Glarus',
-    'CH-GR' => 'Switzerland - Graubünden',
-    'CH-JU' => 'Switzerland - Jura',
-    'CH-LU' => 'Switzerland - Lucerne',
-    'CH-NE' => 'Switzerland - Neuchâtel',
-    'CH-NW' => 'Switzerland - Nidwalden',
-    'CH-OW' => 'Switzerland - Obwalden',
-    'CH-SG' => 'Switzerland - St. Gallen',
-    'CH-SH' => 'Switzerland - Schaffhausen',
-    'CH-SO' => 'Switzerland - Solothurn',
-    'CH-SZ' => 'Switzerland - Schwyz',
-    'CH-TG' => 'Switzerland - Thurgau',
-    'CH-TI' => 'Switzerland - Ticino',
-    'CH-UR' => 'Switzerland - Uri',
-    'CH-VD' => 'Switzerland - Vaud',
-    'CH-VS' => 'Switzerland - Valais',
-    'CH-ZG' => 'Switzerland - Zug',
-    'CH-ZH' => 'Switzerland - Zurich',
+    'CH' => 'Svizzera',
+    'CH-AG' => 'Svizzera - Argovia',
+    'CH-AI' => 'Svizzera - Appenzello Interno',
+    'CH-AR' => 'Svizzera - Appenzello Esterno',
+    'CH-BE' => 'Svizzera - Berna',
+    'CH-BL' => 'Svizzera - Basilea Campagna',
+    'CH-BS' => 'Svizzera - Basilea Città',
+    'CH-FR' => 'Svizzera - Friburgo',
+    'CH-GE' => 'Svizzera - Ginevra',
+    'CH-GL' => 'Svizzera - Glarona',
+    'CH-GR' => 'Svizzera - Grigioni',
+    'CH-JU' => 'Svizzera - Giura',
+    'CH-LU' => 'Svizzera - Lucerna',
+    'CH-NE' => 'Svizzera - Neuchâtel',
+    'CH-NW' => 'Svizzera - Nidvaldo',
+    'CH-OW' => 'Svizzera - Obvaldo',
+    'CH-SG' => 'Svizzera - San Gallo',
+    'CH-SH' => 'Svizzera - Sciaffusa',
+    'CH-SO' => 'Svizzera - Soletta',
+    'CH-SZ' => 'Svizzera - Svitto',
+    'CH-TG' => 'Svizzera - Turgovia',
+    'CH-TI' => 'Svizzera - Ticino',
+    'CH-UR' => 'Svizzera - Uri',
+    'CH-VD' => 'Svizzera - Vaud',
+    'CH-VS' => 'Svizzera - Vallese',
+    'CH-ZG' => 'Svizzera - Zugo',
+    'CH-ZH' => 'Svizzera - Zurigo',
 
     // Turkey
-    'TR' => 'Turkey',
+    'TR' => 'Turchia',
 
     // Ukraine
-    'UA' => 'Ukraine',
+    'UA' => 'Ucraina',
 
     // United Kingdom
-    'GB' => 'United Kingdom',
-    'GB-NIR' => 'United Kingdom - Northern Ireland',
-    'GB-SCT' => 'United Kingdom - Scotland',
+    'GB' => 'Regno Unito',
+    'GB-NIR' => 'Regno Unito - Irlanda del Nord',
+    'GB-SCT' => 'Regno Unito - Scozia',
 
     // USA
-    'US' => 'United States',
+    'US' => 'Stati Uniti',
 ];
 

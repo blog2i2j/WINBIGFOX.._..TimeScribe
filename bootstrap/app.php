fad5997cb56d18acd206b011af876a021d6ee6ca
WINBIGFOX <winbigfox@t-online.de>
2026-01-03 14:32:05 +0100

    fix: exclude `fly-timer/*` from CSRF validation in middleware configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,8 +19,9 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
-        //
+        $middleware->validateCsrfTokens(except: [
+            'fly-timer/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->dontReport([
